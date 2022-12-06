9505553d58f0683c89dc3b16707cdd6f2681778f
Collin <collin@Zipcoders-MacBook-Pro.local>
2022-12-06 12:47:09 -0500

    connection established

--- src/main/java/daos/CarDao.java
+++ src/main/java/daos/CarDao.java
@@ -3,10 +3,15 @@ package daos;
 import models.Car;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CarDao implements DaoInterface {
     Connection connection = ConnectionDriver.getConnection();
+
+    public CarDao() {
+    }
+
     public Car findById(int id) {
         return null;
     }
--- src/main/java/daos/ConnectionDriver.java
+++ src/main/java/daos/ConnectionDriver.java
@@ -1,6 +1,6 @@
 package daos;
 
-import com.mysql.jdbc.Driver;
+import com.mysql.cj.jdbc.Driver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,16 +9,20 @@ import java.sql.SQLException;
 public class ConnectionDriver {
     static String username = "collin1";
     static String password = "zipcode0";
-    static String dbUrl = "jdbc:mysql://localhost.3306/JDBC_DAO_LAB";
+    static String dbUrl = "jdbc:mysql://localhost:3306/JDBC_DAO_LAB";
 
-    public static Connection getConnection() {
-        try {
-            DriverManager.registerDriver(new Driver());
-            System.out.println("Connection Successful!!!");
-            return DriverManager.getConnection(dbUrl, username, password);
-        } catch (SQLException exception) {
-            System.out.println("Error connecting with the database!!!");
-        }
-        return null;
+    public static void main(String[] args) {
+        Connection connection = ConnectionDriver.getConnection();
+        if (connection != null) {System.out.println("Successfully connected");}
     }
-}
+
+        public static Connection getConnection () {
+            try {
+                DriverManager.registerDriver(new Driver());
+                System.out.println("Connection processing...");
+                return DriverManager.getConnection(dbUrl, username, password);
+            } catch (SQLException e) {
+                throw new RuntimeException("Error connecting with the database!!!", e);
+            }
+        }
+    }
